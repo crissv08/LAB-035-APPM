30509a706a2dea913617ed0069f833e9a143903d
Cristofer Salazar <cristofer.r.salazar.v@gmail.com>
2024-01-29 21:34:57 -0600

    Avance clase
    no corregido, profesor menciono que lo haria de nuevo en maquina del salon

--- ionic-basic/src/app/presupuesto/presupuesto.page.ts
+++ ionic-basic/src/app/presupuesto/presupuesto.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PresupuestoService } from '../service/presupuesto.service';
+import { Gasto } from '../interface/gasto';
 
 @Component({
   selector: 'app-presupuesto',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PresupuestoPage implements OnInit {
 
-  constructor() { }
+  public gastos: string[]=['Directos','Fijos', 'Variables'];
+  public selectedValue: any;
+  public monto: number =0;
+  public resultados: string = '';
+  public errResultados: string='light';
+  public gastosList:Gasto[]=[];
+
+  constructor(private presupuestoService : PresupuestoService) { }
 
   ngOnInit() {
+    this.gastosList = this.presupuestoService.mostrarGastos();
   }
+
+  customPopoverOptions: any = {
+    header: 'Seleccion de gastos',
+    subHeader: 'Seleccione el tipo de gasto',
+    message: 'Solo seleccione un tipo de gasto'
+  };
+
+  cambioValor(value: any){
+    console.log(value);
+  }
+
+  guardar(){
+    this.resultados ="";
+    if(this.monto!=null && this.selectedValue!=null){
+      this.errResultados = 'success';
+      this.resultados = 'Gasto seleccionado: '+this.selectedValue+' \nMonto: '+this.monto+'\n';
+      this.presupuestoService.agregarGasto(this.monto, this.selectedValue);
+      this.gastosList = this.presupuestoService.mostrarGastos();
+      this.limpiarCampos();
+    }
+    else{
+      this.errResultados = 'danger';
+      this.resultados ="No a completado los campos del formulario";
+    }
+  } 
+
+  limpiarCampos(){
+    this.monto =0;
+    this.selectedValue =null;
+  }
+
 
 }
